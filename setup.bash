db567952c83860b32e8863b91da6bb16df1e4300
shayer <shayerqh11@gmail.com>
2020-05-30 00:38:33 -0500

    working installer

--- setup.bash
+++ setup.bash
@@ -1,10 +1,11 @@
 #!/bin/bash
+export REF=$(pwd)
+echo "$REF"
 sudo pip install wstool
 sudo apt-get update
-cd ../..
+cd ..
 catkin config --cmake-args -DCMAKE_BUILD_TYPE=Release -DGTSAM_USE_SYSTEM_EIGEN=ON
 catkin config --merge-devel
-cd src
 git clone https://github.com/MIT-SPARK/Kimera-VIO-ROS.git
 wstool init
 wstool merge Kimera-VIO-ROS/install/kimera_vio_ros_https.rosinstall
@@ -15,18 +16,19 @@ cd eig_build
 git clone https://gitlab.com/libeigen/eigen.git ~
 cmake ~/eigen 
 sudo make install
-
+cd "$REF"
 cd ..
+
 sudo rm -r dbow2_catkin
-git clone https://github.com/uzh-rpg/dbow2_catkin.git
+git clone https://github.com/shinsumicco/DBoW2.git
 cd DBoW2
 cmake . 
 make
 sudo make install 
+cd ..
 
-catkin clean --y
-roscd gtsam
+cd gtsam
 git checkout develop
 cd ..
 catkin build opengv_catkin 
-catkin build kimera_vio_ros -DCMAKE_BUILD_TYPE=Release -DGTSAM_USE_SYSTEM_EIGEN=ON
+catkin build -DCMAKE_BUILD_TYPE=Release -DGTSAM_USE_SYSTEM_EIGEN=ON
